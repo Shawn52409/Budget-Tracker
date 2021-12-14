8ff261a8c5db2c67799fe0a90342f28fade37181
Shawn Miller <shawnsmm@gmail.com>
2021-12-14 15:58:54 -0500

    fixing bugs with service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,73 +5,77 @@ const FILES_TO_CACHE = [
     '/styles.css',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
-  ];
+];
   
-  const PRECACHE = 'precache-v1';
-  const RUNTIME_CACHE = 'runtime';
-  
-  self.addEventListener('install', (event) => {
+const PRECACHE = 'precache-v1';
+const RUNTIME_CACHE = 'runtime';
+
+self.addEventListener('install', (event) => {
     event.waitUntil(
-      caches
+        caches
         .open(PRECACHE)
         .then((cache) => cache.addAll(FILES_TO_CACHE))
         .then(self.skipWaiting())
     );
-  });
-  
-  // The activate handler takes care of cleaning up old caches.
-  self.addEventListener('activate', (event) => {
+console.log('Service Worker Installed')
+});
+
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', (event) => {
     const currentCaches = [PRECACHE, RUNTIME_CACHE];
     event.waitUntil(
-      caches
+        caches
         .keys()
         .then((cacheNames) => {
-          return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+            return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
         })
         .then((cachesToDelete) => {
-          return Promise.all(
-            cachesToDelete.map((cacheToDelete) => {
-              return caches.delete(cacheToDelete);
-            })
-          );
+            return Promise.all(
+                cachesToDelete.map((cacheToDelete) => {
+                    return caches.delete(cacheToDelete);
+                })
+            );
         })
         .then(() => self.clients.claim())
     );
-  });
-  
-  self.addEventListener('fetch', (event) => {
+console.log('Service Worker Activated')
+});
+
+// Fetch
+self.addEventListener('fetch', (event) => {
     if (
         event.request.method !== 'GET' ||
-        event.request.url.startsWith(self.location.origin)
+        !event.request.url.startsWith(self.location.origin)
     ) {
         event.respondWith(fetch(event.request));
         return;
     }
 
-  
-    if (event.request.url.includes('/api')) {
+
+    if (event.request.url.includes('/api/')) {
     // make network request and fallback to cache if network request fails (offline)
         event.respondWith(
-            caches.open(RUNTIME_CACHE).then(cache => {
+            caches
+            .open(RUNTIME_CACHE).then(cache => {
                 return fetch(event.request)
-                .then(response => {
-                    cache.put(event.request, response.clone());
-                    return response;
-                })
-                .catch(() => caches.match(event.request));
+            .then(response => {
+                cache.put(event.request, response.clone());
+                return response;
+            })
+            .catch(() => caches.match(event.request));
             })
         );
         return;
     }
 
-  // use cache first for all other requests for performance
+// use cache first for all other requests for performance
     event.respondWith(
         caches.match(event.request).then(cachedResponse => {
             if (cachedResponse) {
                 return cachedResponse;
             }
 
-        // request is not in cache. make network request and cache the response
+        // If request is not in cache then make network request and cache the response
             return caches.open(RUNTIME_CACHE).then(cache => {
                 return fetch(event.request).then(response => {
                     return cache.put(event.request, response.clone()).then(() => {
